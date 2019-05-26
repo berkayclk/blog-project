d26dd2803253a5842200443ac472e8802f8e830f
berkay.celik <berkayc96@hotmail.com>
2019-05-27 00:35:50 +0300

    feat: create pre operations to detect change and encrypt password.

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,7 @@ var validator = require("validator");
 
 const AuthUtil = require('../utility/AuthUtil');
 const ApplicationConstants = require('../constants/ApplicationConstants');
+const CryptoUtil = require("../utility/CryptoUtil");
 const UserUtil = require('../utility/UserUtil');
 
 
@@ -51,6 +52,41 @@ userSchema.set('toJSON', {
         return ret;
     }
 });
+
+userSchema.pre("save", function(next){
+    var user = this;
+    encryptPassword(user,next);
+            
+});
+userSchema.pre("findOneAndUpdate", function(next){
+
+    var user = this.getUpdate();
+    encryptPassword(user,next);
+});
+userSchema.pre("updateOne", function(next){
+
+    var user = this.getUpdate();
+    encryptPassword(user,next);
+});
+userSchema.pre("updateMany", function(next){
+
+    var user = this.getUpdate();
+    encryptPassword(user,next);
+});
+
+function encryptPassword(user,next){
+    
+    if(!user.Password || ( user.isModified && !user.isModified("Password") ) )
+        return next();
+
+    CryptoUtil.encrypt(user.Password)
+                    .then(encrypted =>{
+                        user.Password = encrypted;
+                        next();
+                    })
+                    .catch(err=>{next();});
+        
+}
 
 const User = mongodb.model('User', userSchema);
 
