5240764be15c45d8ad48a1d86a734c2c977ea016
berkay.celik <berkayc96@hotmail.com>
2019-05-26 05:21:07 +0300

    feat: creates User schema as a mongodb model to keep User information

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const mongodb = require('../database/mongodb');
+const AuthUtil = require('../utility/AuthUtil');
+
+var userSchema = mongodb.Schema({
+
+    Fullname:{type:String,required:true,trim:true},
+    Username:{type:String,required:true,trim:true,unique:true},
+    Password:{type:String,required:true,trim:true,minLength:8},
+    Contact:{
+        Phone:{type:String,trim:true},
+        Email:{type:String,trim:true,required:true}
+    },
+    CreateDate:{type:Date,default:Date.now},
+    Follower:[{
+        id: mongodb.Schema.Types.ObjectId
+    }],
+    Following:[{
+        id: mongodb.Schema.Types.ObjectId
+    }],
+    AuthTokens:[{
+        expireDate:{type:Date,required:true, default: AuthUtil.getExpireDate},
+        token:{type:String,required:true}
+    }]
+});
+
+const User = mongodb.model('User', userSchema);
+
+module.exports = User;
