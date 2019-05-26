d103efbf249e75a3f5453af6e794b37fc4134cef
berkay.celik <berkayc96@hotmail.com>
2019-05-26 16:54:29 +0300

    feat: create http response codes as constant

---
+++ src/constants/HttpResponseCodes.js
@@ -0,0 +1,11 @@
+let HttpResponseCode = {
+    OK: 200,
+    CREATED: 201,
+    ACCEPTED: 202,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN:403,
+    DATA_NOT_FOUND:404
+}
+
+module.exports = Object.freeze(HttpResponseCode);;
