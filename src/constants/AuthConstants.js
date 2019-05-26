52a551c60eb3f8e3b04e7af6d76e535609a558b8
berkay.celik <berkayc96@hotmail.com>
2019-05-27 00:44:10 +0300

    feat: create generateToken function in AuthUtil

--- src/constants/AuthConstants.js
+++ src/constants/AuthConstants.js
@@ -1,6 +1,7 @@
 let AuthConstants = {
     AUTHORIZED_DURATION: process.env.AUTHORIZED_DURATION || 7,
-    AUTHORIZED_DURATION_TYPE: process.env.AUTHORIZED_DURATION_TYPE || "DAY"
+    AUTHORIZED_DURATION_TYPE: process.env.AUTHORIZED_DURATION_TYPE || "DAY",
+    JWT_SECRET: process.env.JWT_SECRET || "secret"
 };
 
 module.exports = Object.freeze(AuthConstants);
--- src/utility/AuthUtil.js
+++ src/utility/AuthUtil.js
@@ -1,8 +1,16 @@
 var AuthConstants = require("../constants/AuthConstants");
 const DateUtil = require('../utility/DateUtil');
+const jwt = require('jsonwebtoken');
 
 module.exports.getExpireDate = () => {
     var authorizedDurationType = DateUtil.DURATION_TYPES[AuthConstants.AUTHORIZED_DURATION_TYPE]; 
     var tokenExpireTime = AuthConstants.AUTHORIZED_DURATION * authorizedDurationType;
     return DateUtil.addTimeToNow( tokenExpireTime );
 }
+
+module.exports.generateToken = function (User) {
+    var expireDate = getExpireDate();
+    var token = jwt.sign({ _id: User._id.toHexString(), expireDate }, AuthConstants.JWT_SECRET).toString();
+    var tokenModel = { expireDate, token };
+    return tokenModel;
+};
