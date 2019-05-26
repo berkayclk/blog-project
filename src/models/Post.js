4b5c8a1054ea11c6ea520e5834e6b3d6d9bb27c1
berkay.celik <berkayc96@hotmail.com>
2019-05-26 23:34:28 +0300

    fix: changes enum of Status for post schema.

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,12 +1,13 @@
 const mongodb = require('../database/mongodb');
 
 const PostStatus = require("../constants/PostConstants").PostStaus;
+
 var postSchema = mongodb.Schema({
     Header:{type:String,require:true,trim:true},
     Body:{type:String,require:true,trim:true},
     Author:{type:mongodb.Schema.Types.ObjectId, require:true},
     Location:{type:String, trim:true},
-    Status:{type:String,enum:["private",]}
+    Status:{type:String, enum:[PostStatus.PUBLIC,PostStatus.PRIVATE], default:PostStatus.PUBLIC},
     CreateDate:{type:Date,default:Date.now}
 });
 
