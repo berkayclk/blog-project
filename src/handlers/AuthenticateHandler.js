fef12662a258a706b37489054ec1ff6014f9aad9
berkay.celik <berkayc96@hotmail.com>
2019-05-28 00:34:29 +0300

    fix: adds return statement after send response to client.

--- src/handlers/AuthenticateHandler.js
+++ src/handlers/AuthenticateHandler.js
@@ -7,15 +7,25 @@ var AuthUtil = require("../utility/AuthUtil");
 function checkToken(req,res,next){
 
     const token = AuthUtil.getTokenFromHeader(req);
-    if( !token )
+    if( !token ){
         res.status(HttpResponseCodes.UNAUTHORIZED).send();
-    
+        return;
+    }
+
     var tokenModel = AuthUtil.parseToken(token);
-    if( Date.now() > tokenModel.expireDate )
+    if( Date.now() > tokenModel.expireDate ){
         res.status(HttpResponseCodes.UNAUTHORIZED).send( new ApiResponse("Token is Expired!") );
-    
+        return;
+    }
+
     UserService.findByToken(tokenModel._id, token)
                             .then(AuthUser => {
+                                
+                                if( !AuthUser ){
+                                     res.status(HttpResponseCodes.UNAUTHORIZED).send();
+                                     return;
+                                }
+
                                 req.AuthUser = AuthUser;
                                 req.Token = token;
                                 next();
