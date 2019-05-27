89f356ee45f44d7f087b6ad05fdad7e1b20dc3da
berkay.celik <berkayc96@hotmail.com>
2019-05-28 01:43:27 +0300

    fix: wrap all of authanticate function with try-catch

--- src/handlers/AuthenticateHandler.js
+++ src/handlers/AuthenticateHandler.js
@@ -6,44 +6,50 @@ var LogUtil = require("../utility/LogUtil");
 
 function checkToken(req,res,next){
 
-    const token = AuthUtil.getTokenFromHeader(req);
-    if( !token ){
-        LogUtil.LogError("checkToken - "+ req.url +" - There is no token in Header! ");
+    try {
+        
+        const token = AuthUtil.getTokenFromHeader(req);
+        if( !token ){
+            LogUtil.LogError("checkToken - "+ req.url +" - There is no token in Header! ");
+            res.status(HttpResponseCodes.UNAUTHORIZED).send();
+            return;
+        }
+    
+        var tokenModel = AuthUtil.parseToken(token);
+        if( !tokenModel )
+        {
+            LogUtil.LogError("checkToken - "+ req.url +" - There is no valid token in Header! ");
+            res.status(HttpResponseCodes.UNAUTHORIZED).send();
+            return;
+        }
+    
+        if( Date.now() > tokenModel.expireDate ){
+            LogUtil.LogError("checkToken - "+ req.url +" - Token is Expired!");
+            res.status(HttpResponseCodes.UNAUTHORIZED).send( new ApiResponse("Token is Expired!") );
+            return;
+        }
+    
+        UserService.findByToken(tokenModel._id, token)
+                                .then(AuthUser => {
+                                    
+                                    if( !AuthUser ){
+                                         res.status(HttpResponseCodes.UNAUTHORIZED).send();
+                                         return;
+                                    }
+    
+                                    req.AuthUser = AuthUser;
+                                    req.Token = token;
+                                    next();
+                                })
+                                .catch( err => {
+                                    LogUtil.LogError("checkToken - "+ req.url +" - "+ err);
+                                    res.status(HttpResponseCodes.UNAUTHORIZED).send(); 
+                                });
+
+    }catch(err){
+        LogUtil.LogError("checkToken - "+ req.url +" - "+ err);
         res.status(HttpResponseCodes.UNAUTHORIZED).send();
-        return;
     }
-
-    var tokenModel = AuthUtil.parseToken(token);
-    if( !tokenModel )
-    {
-        LogUtil.LogError("checkToken - "+ req.url +" - There is no valid token in Header! ");
-        res.status(HttpResponseCodes.UNAUTHORIZED).send();
-        return;
-    }
-
-    if( Date.now() > tokenModel.expireDate ){
-        LogUtil.LogError("checkToken - "+ req.url +" - Token is Expired!");
-        res.status(HttpResponseCodes.UNAUTHORIZED).send( new ApiResponse("Token is Expired!") );
-        return;
-    }
-
-    UserService.findByToken(tokenModel._id, token)
-                            .then(AuthUser => {
-                                
-                                if( !AuthUser ){
-                                     res.status(HttpResponseCodes.UNAUTHORIZED).send();
-                                     return;
-                                }
-
-                                req.AuthUser = AuthUser;
-                                req.Token = token;
-                                next();
-                            })
-                            .catch( err => {
-                                LogUtil.LogError("checkToken - "+ req.url +" - "+ err);
-                                res.status(HttpResponseCodes.UNAUTHORIZED).send(); 
-                            })
-
 }
 
 module.exports = checkToken;
