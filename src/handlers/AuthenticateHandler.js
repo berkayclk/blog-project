81d0632654cd81bc180485e74ca4c7b14d8e2d07
berkay.celik <berkayc96@hotmail.com>
2019-05-27 03:08:01 +0300

    feat: create AuthanticateHandler to atuhanticate request.

---
+++ src/handlers/AuthenticateHandler.js
@@ -0,0 +1,27 @@
+var UserService = require("../services/UserService");
+var HttpResponseCodes = require("../constants/HttpResponseCodes");
+var ApiResponse = require("../models/ApiResponse");
+var AuthUtil = require("../utility/AuthUtil");
+
+
+function checkToken(req,res,next){
+
+    const token = AuthUtil.getTokenFromHeader(req);
+    if( !token )
+        res.status(HttpResponseCodes.UNAUTHORIZED).send();
+    
+    var tokenModel = AuthUtil.parseToken(token);
+    if( Date.now() > tokenModel.expireDate )
+        res.status(401).send( new ApiResponse("Token is Expired!") );
+    
+    UserService.findByToken(tokenModel._id, token)
+                            .then(AuthUser => {
+                                req.AuthUser = AuthUser;
+                                req.Token = token;
+                                next();
+                            })
+                            .catch( err => res.status(401).send( new ApiResponse(err)) )
+
+}
+
+module.exports = checkToken;
