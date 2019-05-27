a6a24910701e3e40ed0786b786130084c70a79fc
berkay.celik <berkayc96@hotmail.com>
2019-05-28 01:44:34 +0300

    fix: chnages require as required in PostSchema

--- src/models/Post.js
+++ src/models/Post.js
@@ -3,9 +3,9 @@ const mongodb = require('../database/mongodb');
 const PostStatus = require("../constants/PostConstants").PostStaus;
 
 var postSchema = mongodb.Schema({
-    Header:{type:String,require:true,trim:true},
-    Body:{type:String,require:true,trim:true},
-    Author:{type:mongodb.Schema.Types.ObjectId, require:true},
+    Header:{type:String,required:true,trim:true},
+    Body:{type:String,required:true,trim:true},
+    Author:{type:mongodb.Schema.Types.ObjectId, required:true},
     Location:{type:String, trim:true},
     Status:{type:String, enum:[PostStatus.PUBLIC,PostStatus.PRIVATE], default:PostStatus.PUBLIC},
     CreateDate:{type:Date,default:Date.now}
