347463b7d4af3f87087ae434e8c5197dc566ac68
berkay.celik <berkayc96@hotmail.com>
2019-05-28 00:01:33 +0300

    refactor: use colors module to wirte console with color in LogUtil

--- src/utility/LogUtil.js
+++ src/utility/LogUtil.js
@@ -1,14 +1,16 @@
+const colors = require('colors/safe');
+
 module.exports.LogInfo = (text) =>{
     var now = new Date();
-    console.error( "\x1b[34m",`[INFO] -- ${now.toLocaleString()} -- ${text}`);
+    console.error( colors.blue(`[INFO] -- ${now.toLocaleString()} -- ${text}`));
 };
 
 module.exports.LogWarning = (text) =>{
     var now = new Date();
-    console.error( "\x1b[33m",`[WARNING] -- ${now.toLocaleString()} -- ${text}`);
+    console.error( colors.yellow(`[WARNING] -- ${now.toLocaleString()} -- ${text}`));
 };
 
 module.exports.LogError = (text) =>{
     var now = new Date();
-    console.error( "\x1b[31m",`[ERROR] -- ${now.toLocaleString()} -- ${text}`);
+    console.error( colors.red(`[ERROR] -- ${now.toLocaleString()} -- ${text}`));
 };
